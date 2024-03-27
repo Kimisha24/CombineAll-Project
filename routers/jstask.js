5dc20cf7542b684f58da837887dc3b19ac0e5b75
Kimisha Patel <kimisha.patel2024@gmail.com>
2024-03-27 19:15:34 +0530

    adding kukucube

--- routers/jstask.js
+++ routers/jstask.js
@@ -4,7 +4,10 @@ function route(app) {
         res.sendFile('/home/kimisha-ladani/Documents/project-combinepractice/views/Js-task/dynamictable.html');
     });
 
-    
+    app.get('/Kukucube', (req, res) => {
+        res.sendFile('/home/kimisha-ladani/Documents/project-combinepractice/views/Js-task/Kukucube.html');
+    });
+
 
 }
 
