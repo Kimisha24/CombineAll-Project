e3b6bff6a29c7c10db8441a834bc5be92b78afc2
Kimisha Patel <kimisha.patel2024@gmail.com>
2024-03-27 19:35:59 +0530

    adding jsevents and sort

--- routers/jstask.js
+++ routers/jstask.js
@@ -12,8 +12,13 @@ function route(app) {
         res.sendFile('/home/kimisha-ladani/Documents/project-combinepractice/views/Js-task/tictactoe.html');
     });
 
-
-
+    app.get('/jsevent', (req, res) => {
+        res.sendFile('/home/kimisha-ladani/Documents/project-combinepractice/views/Js-task/jsevent.html');
+    });
+    
+    app.get('/Bubblesort', (req, res) => {
+        res.sendFile('/home/kimisha-ladani/Documents/project-combinepractice/views/Js-task/sort.html');
+    });
 }
 
 module.exports = route;
